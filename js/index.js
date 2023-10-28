ae23d64b6dc27b05e4e39a095e5c39f9e2072624
GILLMtz <martinezgomezgil@hotmail.com>
2023-10-27 19:26:42 -0600

    create function to create test tasks

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 import  App  from "./app.js";
+import { inputTask } from "./constantes.js";
+import actionHandler from "./action-handler.js";
 
 App.init();
+
+function mock() {
+    const nTasks = 20;
+    for (let i = 0; i <= nTasks; i++) {
+        inputTask.value=`Task ${i}`;
+        actionHandler.add(inputTask.value);
+    }
+}
+mock();
